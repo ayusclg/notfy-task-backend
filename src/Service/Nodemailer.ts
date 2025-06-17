9bfcd630af828e2cf374544a4e06d3c4ce95f4c8
ayusclg <freelancerayus@gmail.com>
2025-06-17 19:50:28 +0545

    Auth controllers & multer setup

---
+++ src/Service/Nodemailer.ts
@@ -0,0 +1,46 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv'
+export interface mail {
+  to: string;
+  html: string;
+  subject: string;
+    text: string;
+
+}
+
+dotenv.config()
+const user = process.env.EMAIL_USER as string;
+const pass = process.env.EMAIL_PASS as string;
+
+console.log(user,"user")
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user,
+    pass,
+  },
+});
+
+
+ const sendMail = async ({ to, html, subject, text }: mail): Promise<string> => {
+  const mailOptions = {
+    from: user,
+    to,
+    subject,
+    html,
+    text,
+
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    return info.messageId;
+  } catch (error) {
+    console.error("Could not send mail:", error);
+    throw new Error("Mail sending failed");
+  }
+};
+
+export {sendMail}
